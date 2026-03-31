4109b31642eea94dc5691b74712140194ad17e29
Damien Rodriguez <damien.rodriguez@mailfence.com>
2026-03-31 13:47:43 -0700

    added mock data stuf, not working

--- mock-log-generator/src/main.rs
+++ mock-log-generator/src/main.rs
@@ -11,12 +11,49 @@ The log files will look like the following:
 Malformed log entries can be anything really.
  */
 
-use rand::prelude::*;
+use chrono::{DateTime, Duration, Utc};
+use rand::Rng;
+
+enum LogType {
+    INFO,
+    WARN,
+    DEBUG,
+    ERROR,
+}
+
+const SYSTEM_TYPES: [&str; 7] = [
+    "auth-service",
+    "payment-service",
+    "user-service",
+    "api-gateway",
+    "notification-service",
+    "system",
+    "cache-service",
+];
+
+struct LogMessage {
+    date_time: DateTime<Utc>,
+    log_type: LogType,
+    system: String,
+    action: String,
+    misc: String,
+}
 
 fn main() {
     println!("Hello, world!");
-    let mut rng = rand::rng();
+}
 
-    println!("char: '{}'", rng.random::<char>());
-    println!("alpha: '{}'", rng.sample(rand::distr::Alphanumeric) as char);
+fn random_datetime_in_range(
+    rng: &mut impl Rng,
+    start: DateTime<Utc>,
+    end: DateTime<Utc>,
+) -> DateTime<Utc> {
+    let duration = end - start;
+    let random_seconds = rng.gen_range(0..duration.num_seconds());
+    start + Duration::seconds(random_seconds)
+}
+
+fn random_log_type(rng: &mut impl Rng) -> String {
+    let index = rng.gen_range(0..SYSTEM_TYPES.len());
+    SYSTEM_TYPES[index]
 }
